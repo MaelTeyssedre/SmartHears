b8618c1b6af19ccc3331bde22467eb3ae7c67145
MT <maelteyssedre@gmail.com>
2022-07-15 21:02:33 +0200

    registered the chatRepository in getit

--- smarthears_app/lib/main.dart
+++ smarthears_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:smarthears_app/app.dart';
 import 'package:smarthears_app/repositories/authentication_repository.dart';
+import 'package:smarthears_app/repositories/chat_repository.dart';
 import 'package:smarthears_app/repositories/user_repository.dart';
 
 GetIt getIt = GetIt.instance;
@@ -9,6 +10,7 @@ GetIt getIt = GetIt.instance;
 void main() async {
   getIt.registerSingleton<AuthenticationRepository>(AuthenticationRepository('URL'), signalsReady: true);
   getIt.registerSingleton<UserRepository>(UserRepository(), signalsReady: true);
+  getIt.registerSingleton<ChatRepository>(ChatRepository(), signalsReady: true);
   // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const SmartHears());
 }
