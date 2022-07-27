dfa47eb538036f5ea43c563ceb27b1a5667ce06a
Alban Bodenes <alban.bodenes@epitech.eu>
2022-07-27 17:34:54 +0200

    Implement bt device list on the Smarthears app home page

--- smarthears_app/lib/main.dart
+++ smarthears_app/lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:get_it/get_it.dart';
 import 'package:smarthears_app/app.dart';
+import 'package:smarthears_app/bt_device_tile.dart';
 import 'package:smarthears_app/repositories/authentication_repository.dart';
+
+import 'dart:async';
+import 'dart:math';
 
 GetIt getIt = GetIt.instance;
 
 void main() async {
-  getIt.registerSingleton<AuthenticationRepository>(AuthenticationRepository('URL'), signalsReady: true);
+  getIt.registerSingleton<AuthenticationRepository>(
+      AuthenticationRepository('URL'),
+      signalsReady: true);
+
   // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const SmartHears());
 }
--- smarthears_app/lib/modules/home/home_page.dart
+++ smarthears_app/lib/modules/home/home_page.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:smarthears_app/models/theme.dart';
+import 'package:smarthears_app/bt_device_tile.dart';
 import 'package:smarthears_app/modules/dashboard/view/dashboard_screen.dart';
 import 'package:smarthears_app/modules/splash/splash.dart';
+
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+
+import 'dart:math';
 
 class HomePage extends StatefulWidget {
   static const String route = '/home';
@@ -36,22 +42,176 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Scaffold(
-        extendBody: true,
-        bottomNavigationBar: CurvedNavigationBar(
-            height: 70,
-            key: _bottomNavigationKey,
-            backgroundColor: theme.colorScheme.background,
-            color: theme.primaryColor,
-            buttonBackgroundColor: theme.colorScheme.secondary,
-            index: _page,
-            items: [
-              ShaderMask(shaderCallback: shaderCallback, child: const Icon(Icons.home, size: 30)),
-              ShaderMask(shaderCallback: shaderCallback, child: const Icon(Icons.account_circle, size: 30)),
-              ShaderMask(shaderCallback: shaderCallback, child: const Icon(Icons.search, size: 30)),
-              ShaderMask(shaderCallback: shaderCallback, child: const Icon(Icons.chat_bubble, size: 30))
+      extendBody: true,
+      bottomNavigationBar: CurvedNavigationBar(
+          height: 70,
+          key: _bottomNavigationKey,
+          backgroundColor: theme.colorScheme.background,
+          color: theme.primaryColor,
+          buttonBackgroundColor: theme.colorScheme.secondary,
+          index: _page,
+          items: [
+            ShaderMask(
+                shaderCallback: shaderCallback,
+                child: const Icon(Icons.home, size: 30)),
+            ShaderMask(
+                shaderCallback: shaderCallback,
+                child: const Icon(Icons.account_circle, size: 30)),
+            ShaderMask(
+                shaderCallback: shaderCallback,
+                child: const Icon(Icons.search, size: 30)),
+            ShaderMask(
+                shaderCallback: shaderCallback,
+                child: const Icon(Icons.chat_bubble, size: 30))
+          ],
+          animationDuration: const Duration(milliseconds: 300),
+          onTap: (index) => setState(() => _page = index)),
+
+
+      body: RefreshIndicator(
+        onRefresh: () => FlutterBluePlus.instance
+            .startScan(timeout: const Duration(seconds: 4)),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              StreamBuilder<List<BluetoothDevice>>(
+                stream: Stream.periodic(const Duration(seconds: 2))
+                    .asyncMap((_) => FlutterBluePlus.instance.connectedDevices),
+                initialData: const [],
+                builder: (c, snapshot) => Column(
+                  children: snapshot.data!
+                      .map((d) => ListTile(
+                            title: Text(d.name),
+                            subtitle: Text(d.id.toString()),
+                            trailing: StreamBuilder<BluetoothDeviceState>(
+                              stream: d.state,
+                              initialData: BluetoothDeviceState.disconnected,
+                              builder: (c, snapshot) {
+                                return Text(snapshot.data.toString());
+                              },
+                            ),
+                          ))
+                      .toList(),
+                ),
+              ),
+              StreamBuilder<List<ScanResult>>(
+                stream: FlutterBluePlus.instance.scanResults,
+                initialData: const [],
+                builder: (c, snapshot) => Column(
+                  children: snapshot.data!
+                      .map(
+                        (r) => ScanResultTile(
+                          result: r,
+                          onTap: () => Navigator.of(context)
+                              .push(MaterialPageRoute(builder: (context) {
+                            r.device.connect();
+                            return DeviceScreen(device: r.device);
+                          })),
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
             ],
-            animationDuration: const Duration(milliseconds: 300),
-            onTap: (index) => setState(() => _page = index)),
-        body: Padding(padding: const EdgeInsets.only(bottom: 0), child: getContent(_page)));
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DeviceScreen extends StatelessWidget {
+  const DeviceScreen({Key? key, required this.device}) : super(key: key);
+
+  final BluetoothDevice device;
+
+  List<int> _getRandomBytes() {
+    final math = Random();
+    return [
+      math.nextInt(255),
+      math.nextInt(255),
+      math.nextInt(255),
+      math.nextInt(255)
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(device.name),
+        actions: <Widget>[
+          StreamBuilder<BluetoothDeviceState>(
+            stream: device.state,
+            initialData: BluetoothDeviceState.connecting,
+            builder: (c, snapshot) {
+              VoidCallback? onPressed;
+              return TextButton(
+                  onPressed: onPressed,
+                  child: Text(
+                    '',
+                    style: Theme.of(context)
+                        .primaryTextTheme
+                        .button
+                        ?.copyWith(color: Colors.white),
+                  ));
+            },
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            StreamBuilder<BluetoothDeviceState>(
+              stream: device.state,
+              initialData: BluetoothDeviceState.connecting,
+              builder: (c, snapshot) => ListTile(
+                leading: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                title: Text(
+                    'Device is ${snapshot.data.toString().split('.')[1]}.'),
+                subtitle: Text('${device.id}'),
+                trailing: StreamBuilder<bool>(
+                  stream: device.isDiscoveringServices,
+                  initialData: false,
+                  builder: (c, snapshot) => IndexedStack(
+                    index: snapshot.data! ? 1 : 0,
+                    children: <Widget>[
+                      IconButton(
+                        icon: const Icon(Icons.refresh),
+                        onPressed: () => device.discoverServices(),
+                      ),
+                      const IconButton(
+                        icon: SizedBox(
+                          child: CircularProgressIndicator(
+                            valueColor: AlwaysStoppedAnimation(Colors.grey),
+                          ),
+                          width: 18.0,
+                          height: 18.0,
+                        ),
+                        onPressed: null,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            StreamBuilder<int>(
+              stream: device.mtu,
+              initialData: 0,
+              builder: (c, snapshot) => ListTile(
+                title: const Text('MTU Size'),
+                subtitle: Text('${snapshot.data} bytes'),
+                trailing: IconButton(
+                  icon: const Icon(Icons.edit),
+                  onPressed: () => device.requestMtu(223),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
